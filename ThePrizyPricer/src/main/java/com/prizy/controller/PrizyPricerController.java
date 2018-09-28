0e72817f9fb6c1a7a88032346b315f9a80d17a05
kuldeepjha <kuldeepjha09@gmail.com>
2018-09-28 10:16:44 +0530

    Update PrizyPricerController.java

--- ThePrizyPricer/src/main/java/com/prizy/controller/PrizyPricerController.java
+++ ThePrizyPricer/src/main/java/com/prizy/controller/PrizyPricerController.java
@@ -2,13 +2,30 @@ package com.prizy.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.prizy.service;
 
 @RestController
 public class PrizyPricerController {
 
+    @Autowire
+    PrizyPriceService prizyPriceService;
+    
+    @RequestMapping("/productLoader")
+    public String productLoader(@RequestBody ProductLoaderInDto productLoaderInDto) {
 
-    @RequestMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
+        return prizyPriceService.productLoader(productLoaderInDto);
+    }
+    
+    @RequestMapping("/productLoader")
+    public List<ProductLoaderModel> productList() {
+        
+        return prizyPriceService.productList();
+    }
+    
+    @RequestMapping("/productLoader")
+    public ProductLoaderOutDto productLoader(int id) {
+        
+        return prizyPriceService.productView(id);
     }
 }
